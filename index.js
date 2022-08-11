9455d30673170c28119729e874a28a4a40f99ce0
Timothy <tkkibet13@gmail.com>
2022-08-11 23:30:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(nameArray, event) {
+    const newArray=[]
+    for (let i=0; i<nameArray.length; i++){
+        newArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
+
+function countDown(num) {
+    let countup=num+1;
+    if(num<0){
+        return
+    }
+    while(countup>0){
+        countup--
+        console.log(countup)
+    }
+}
